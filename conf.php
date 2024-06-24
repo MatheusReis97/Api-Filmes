17c973f7c5d4f4fd96a42bd9f846dfda14eb2785
MatheusReis97 <Matheus.drs97@hotmail.com>
2024-06-24 16:28:15 -0300

    Atualizando

---
+++ conf.php
@@ -0,0 +1,7 @@
+<?php
+
+$url = "https://api.themoviedb.org/3/movie/550?api_key=bb00c0fa93d63563a61b1eb6a4a1df7d";
+$url1 = "https://api.themoviedb.org/3/movie/76341?api_key=bb00c0fa93d63563a61b1eb6a4a1df7d";
+$apikey = "bb00c0fa93d63563a61b1eb6a4a1df7d";
+$filmes = json_decode(file_get_contents($url));
+
